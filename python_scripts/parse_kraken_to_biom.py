fc898a5a156b6db8f7b7e48e672987308c2a4163
Jenya Kopylova <jenya.kopylov@gmail.com>
2016-08-20 18:13:54 +0200

    update code to kraken translate

--- python_scripts/parse_kraken_to_biom.py
+++ python_scripts/parse_kraken_to_biom.py
@@ -9,118 +9,90 @@
 #-----------------------------------------------------------------------------
 
 """
-Parse output of kraken-mpa-report and generate single BIOM table.
+Parse output of "kraken translate" and generate single BIOM table.
 """
 
 import sys
 import os
 from os.path import isfile, join
 import random
+import click
+import pandas as pd
 import numpy as np
-import matplotlib as mpl
-mpl.use('Agg')
-# requires matplot version 1.2.1 (or one that supports fontsize in the legend)
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
-import brewer2mpl
+from biom.table import Table
 
 
-def compute_abundances_biom(reports_dp,
-                            metadata_fp,
-                            taxonomy_report_dp,
-                            taxa_level,
+def compute_abundances_biom(kraken_translate_report_fp,
+                            qiime_metadata_fp,
+                            taxonomic_rank,
                             taxa_levels,
-                            taxa_levels_idx):
+                            taxa_levels_idx,
+                            columns,
+                            index):
     """Absolute abundance of number of reads matching a defined taxa level.
 
     Parameters
     ----------
-    reports_dp: string
-        output directory path
-    metadata_fp: string
-        path to TCGA tsv metadata file
-    taxonomy_report_dp: string
-        path to directory with output(s) of kraken-mpa-report
-    taxa_level: string
+    kraken_translate_report_fp: string
+        filepath to output of "kraken translate"
+    qiime_metadata_fp: string
+        filepath to QIIME metadata file
+    taxonomic_rank: string
         taxonomy level (e.g., genus or species)
-    taxa_levels: dictionary
-        "domain": "d__",
-        "phylum": "|p__",
-        "class": "|c__",
-        "order": "|o__",
-        "family": "|f__",
-        "genus": "|g__",
-        "species": "|s__"
-    taxa_levels_idx: dictionary
-        "d__": 0, "|p__": 1, "|c__": 2,
-        "|o__": 3, "|f__": 4, "|g__": 5,
-        "|s__": 6, "6": "|s__", "5": "|g__",
-        "4": "|f__", "3": "|o__", "2": "|c__",
-        "1": "|p__", "0": "d__"
 
     Returns
     -------
-    
+    biom_table: biom.Table
+        BIOM table
     """
     total_levels = len(taxa_levels)
+    # columns are sample IDs and rows are taxonomy strings
+    abundances = pd.DataFrame(columns=columns, index=index)    
+    taxonomic_rank_level_str = taxa_levels[taxonomic_rank]
+    taxonomic_rank_level_int = taxa_levels_idx[taxonomic_rank_level_str]
+    split_on_level = taxa_levels_idx[str(taxonomic_rank_level_int + 1)]
+    with open(kraken_translate_report_fp) as kraken_translate_report_f:
+        for line in kraken_translate_report_f:
+            label, taxonomy = line.strip().split('\t')
+            # record abundance
+            if taxonomic_rank_level_str in taxonomy:
+                # keep taxonomy string up to specified level
+                taxonomy = taxonomy.split(split_on_level)[0]
+                sample_id = label.split('_')[0]
+                value = abundances.at[taxonomy, sample_id]
+                if np.isnan(value):
+                    abundances.set_value(taxonomy, sample_id, 1.)
+                else:
+                    abundances.set_value(taxonomy, sample_id, value+1.)
+    values = abundances.values()
+    #table = Table(abundances.values(), abundances.index.values.tolist(), abundances.columns.values.tolist())
+    #print(table)
+    #return table
 
-    # keys are file names
-    metadata = {}
-    num_samples = 1
-    with open(metadata_fp, 'U') as metadata_f:
-        for line in metadata_f:
-            # skip the header
-            if line.startswith('#'):
-                continue
-            line = line.strip().split("\t")
-            file_n = line[6].split(".bam")[0]
-            if file_n not in metadata:
-                metadata[file_n] = [line[7], line[5], line[9], line[0]]
-                num_samples += 1
 
-    organs_list = sorted({metadata[file_n][1] for file_n in metadata if isfile(join(taxonomy_report_dp, "%s_kraken.report_mpa" % file_n))})
-    print organs_list
-
-    # species abundance
-    abundances = {}
-    labels = []
-    total_organs = len(organs_list)
-    taxa_l = taxa_levels[taxa_level]
-    sample_id = 0
-    for file_n in metadata:
-        kraken_report_fp = join(taxonomy_report_dp, "%s_kraken.report_mpa" % file_n) 
-        if isfile(kraken_report_fp):
-            # record abundances
-            with open(kraken_report_fp, 'U') as kraken_report_f:
-                for line in kraken_report_f:
-                    stop_search = False
-                    #print "%s\t%s" % (taxa_l, line)
-                    # get statistics only for specified taxa level
-                    if taxa_l in line:
-                        # cannot have deeper levels after specified level
-                        for i in range(taxa_levels_idx[taxa_l]+1, total_levels):
-                            if taxa_levels_idx[str(i)] in line:
-                                stop_search = True
-                                break
-                    else:
-                        stop_search = True
-                    if stop_search:
-                        continue
-                    line = line.strip().split()
-                    taxa = line[0]
-                    abundance = np.float128(line[1])
-                    # check if taxa exists
-                    if taxa not in abundances:
-                        abundances[taxa] = []
-                        # new taxa not found previously in any organ,
-                        # add to dictionary and set all abundances for
-                        # other organs to 0.0
-                        for j in range(0, num_samples):
-                            abundances[taxa].append(0.0)
-                    abundances[taxa][sample_id] += abundance
-            sample_id += 1
-
-    return (abundances, organs_list, total_organs, metadata)
+def prepare_dataframe(kraken_translate_report_fp,
+                      taxonomic_rank,
+                      taxa_levels,
+                      taxa_levels_idx):
+    """Return sets for sample IDs and taxonomy strings.
+    """
+    total_levels = len(taxa_levels)
+    taxonomic_rank_level_str = taxa_levels[taxonomic_rank]
+    taxonomic_rank_level_int = taxa_levels_idx[taxonomic_rank_level_str]
+    split_on_level = taxa_levels_idx[str(taxonomic_rank_level_int + 1)]
+    sample_ids = set()
+    taxonomies = set()
+    with open(kraken_translate_report_fp) as kraken_translate_report_f:
+        for line in kraken_translate_report_f:
+            label, taxonomy = line.strip().split('\t')
+            sample_id = label.split('_')[0]
+            sample_ids.add(sample_id)
+            # record abundance
+            if taxonomic_rank_level_str in taxonomy:
+                # keep taxonomy string up to specified level
+                taxonomy = taxonomy.split(split_on_level)[0]
+                taxonomies.add(taxonomy)
+    return sample_ids, taxonomies
 
 
 def output_biom_table(abundances,
@@ -129,7 +101,7 @@ def output_biom_table(abundances,
                       taxa_level,
                       taxonomy_report_dp,
                       metadata):
-    """
+    """Output BIOM table.
     """
     output_biom_fp = join(reports_dp, "tcga_output_%s.biom" % (taxa_level))
     with open(output_biom_fp, 'w') as output_f:
@@ -151,20 +123,27 @@ def output_biom_table(abundances,
             output_f.write("\n")   
 
 
-def main(argv):
-
-    # kraken-mpa-report filepath
-    report_fp = sys.argv[1]
-
-    # metadata TSV file (QIIME format)
-    metadata_fp = sys.argv[2]
-
-    # taxonomy report directory path
-    taxonomy_report_dp = sys.argv[3]
-
-    # taxonomy level (ex. genus, species)
-    taxa_level = sys.argv[4]
-
+@click.command()
+@click.argument('kraken-translate-report-fp', required=True,
+                type=click.Path(resolve_path=True, readable=True, exists=True,
+                                file_okay=True))
+@click.argument('qiime-metadata-fp', required=False,
+                type=click.Path(resolve_path=True, readable=True, exists=True,
+                                file_okay=True))
+@click.option('--taxonomic-rank', type=click.Choice(['genus', 'species',
+                                                     'family', 'order',
+                                                     'class', 'phylum',
+                                                     'domain']),
+              required=False, default=['genus'], show_default=True,
+              help="Taxonomic rank at which to generate summary")
+@click.option('--biom-output-fp', required=False,
+              type=click.Path(resolve_path=True, readable=True, exists=False,
+                              file_okay=True),
+              help="Filepath to output BIOM table (default HDF5 format)")
+def main(kraken_translate_report_fp,
+         qiime_metadata_fp,
+         taxonomic_rank,
+         biom_output_fp):
     taxa_levels = {"domain": "d__",
                    "phylum": "|p__",
                    "class": "|c__",
@@ -172,29 +151,33 @@ def main(argv):
                    "family": "|f__",
                    "genus": "|g__",
                    "species": "|s__"}
-
     taxa_levels_idx = {"d__": 0, "|p__": 1, "|c__": 2,
                        "|o__": 3, "|f__": 4, "|g__": 5,
                        "|s__": 6, "6": "|s__", "5": "|g__",
                        "4": "|f__", "3": "|o__", "2": "|c__",
                        "1": "|p__", "0": "d__"}
-
-    abundances, organs_list, total_organs, metadata =\
+    columns, index = prepare_dataframe(
+        kraken_translate_report_fp=kraken_translate_report_fp,
+        taxonomic_rank=taxonomic_rank,
+        taxa_levels=taxa_levels,
+        taxa_levels_idx=taxa_levels_idx)
+    abundances =\
         compute_abundances_biom(
-            reports_dp=reports_dp,
-            metadata_fp=metadata_fp,
-            taxonomy_report_dp=taxonomy_report_dp,
-            taxa_level=taxa_level,
+            kraken_translate_report_fp=kraken_translate_report_fp,
+            qiime_metadata_fp=qiime_metadata_fp,
+            taxonomic_rank=taxonomic_rank,
             taxa_levels=taxa_levels,
-            taxa_levels_idx=taxa_levels_idx)
+            taxa_levels_idx=taxa_levels_idx,
+            columns=columns,
+            index=index)
 
-    output_biom_table(abundances=abundances,
-                      organs_list=organs_list,
-                      reports_dp=reports_dp,
-                      taxa_level=taxa_level,
-                      taxonomy_report_dp=taxonomy_report_dp,
-                      metadata=metadata)
+    #output_biom_table(abundances=abundances,
+    #                  organs_list=organs_list,
+    #                  reports_dp=reports_dp,
+    #                  taxa_level=taxa_level,
+    #                  taxonomy_report_dp=taxonomy_report_dp,
+    #                  metadata=metadata)
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
