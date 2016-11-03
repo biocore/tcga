85ef3a2fb7c0ccf9f15775720b83745dabd2690f
Jad Kanbar <jkanbar@Jads-MacBook-Pro.local>
2016-11-02 22:47:24 -0700

    Can now add input tuple of krakne mpa reports

--- python_scripts/cgc_bowtie2_build.py
+++ python_scripts/cgc_bowtie2_build.py
@@ -16,66 +16,45 @@ import click
 import collections
 
 def load_kraken_mpa_report(kraken_mpa_report_fp,
-                           taxonomic_rank,
                            taxa_levels,
-                           taxa_levels_idx,
                            read_per_taxa):
     """Absolute abundance of number of reads matching a defined taxa level.
     Parameters
     ----------
     kraken_translate_report_fp: str
         filepath to output of "kraken translate"
-    taxonomic_rank: str
-        taxonomy level (e.g., genus or species)
-    taxa_levels: dict
-        keys are full name taxonomic ranks and values are abbreviated ranks
-    taxa_levels_idx: dict
-        2-way dict storing integer depths for abbreviated taxonomic ranks
-    read_per_taxa: in
+    taxa_levels: list
+        list of two elements that includes the taxonomic rank at which
+        to generate summary and rank below to split by
+    read_per_taxa: int
         integer of number of minimum number of reads to keep per taxa
 
     Returns
     -------
     taxonomies: set
         set of taxonomies from kraken_translate_report_fp
-    split_on_level: str
-        string that determines the level to split the taxonomy to be used in
-        create_db_folder function
-    repo_file_name: str
-        string with filename suffix from kraken_translate_report_fp to be used
-        in create_db_folder function
-
     """
-    repo_file_name = kraken_mpa_report_fp.strip().split('/')[-1].split('mpa')[0]
-
-    taxonomic_rank_level_str = taxa_levels[taxonomic_rank]
-    taxonomic_rank_level_int = taxa_levels_idx[taxonomic_rank_level_str]
-
-    if taxonomic_rank_level_int < 6:
-        split_on_level = taxa_levels_idx[str(taxonomic_rank_level_int + 1)]
-    else:
-        # keep full string (to species level)
-        split_on_level = '\n'
     taxonomic_abundances= []
-    with open(kraken_mpa_report_fp) as kraken_translate_report_f:
-        for line in kraken_translate_report_f:
-            label, taxonomy = line.strip().split('\t')
-            if taxonomic_rank_level_str in taxonomy:
-                # keep taxonomy string up to specified level
-                taxonomy_parse = taxonomy.split(split_on_level)[0]
-                taxonomy_parse = taxonomy_parse.replace('d__','k__')
-                taxonomic_abundances.append(taxonomy_parse)
+    for report_fp in kraken_mpa_report_fp:
+        with open(report_fp) as report_fp:
+            for line in report_fp:
+                label, taxonomy = line.strip().split('\t')
+                if taxa_levels[0] in taxonomy:
+                    # keep taxonomy string up to specified level
+                    taxonomy_parse = taxonomy.split(taxa_levels[1])[0]
+                    taxonomy_parse = taxonomy_parse.replace('d__','k__')
+                    taxonomic_abundances.append(taxonomy_parse)
 
     taxonomies = set([k for k, v in
                       collections.Counter(taxonomic_abundances).iteritems()
                       if v > read_per_taxa])
 
-    return taxonomies, split_on_level, repo_file_name
+    return taxonomies
 
 def create_db_folder(repophlan_genomeid_taxonomy_fp,
                      genome_tax,
                      split_on_level,
-                     repo_file_name):
+                     output_filename):
 
     """Return sets for sample IDs and taxonomy strings.
     Parameters
@@ -86,8 +65,8 @@ def create_db_folder(repophlan_genomeid_taxonomy_fp,
         set of taxonomies from kraken_translate_report_fp
     split_on_level: str
         string that determines the level to split the taxonomy in genome_tax
-    repo_file_name
-        string with filename suffix from kraken_translate_report_fp
+    output_filename: str
+        string with output filename
 
     Returns
     -------
@@ -95,9 +74,7 @@ def create_db_folder(repophlan_genomeid_taxonomy_fp,
     taxa from repophlan_genomeid_taxonomy_fp containned with the set
     genome_tax
     """
-    repo_file_name = repo_file_name+'repophlan_genomeID_taxonomy.good'
-
-    with open(repo_file_name, 'w') as f:
+    with open(output_filename, 'w') as f:
         with open(repophlan_genomeid_taxonomy_fp) as repophlan_genomeID_taxonomy_f:
             for line in repophlan_genomeID_taxonomy_f:
                 tax_id_repo, directory_repo, taxonomy_repo =\
@@ -108,7 +85,8 @@ def create_db_folder(repophlan_genomeid_taxonomy_fp,
 
 
 @click.command()
-@click.option('--kraken-mpa-report-fp', required=True,
+
+@click.option('--kraken-mpa-report-fp', required=True, multiple=True,
               type=click.Path(resolve_path=True, readable=True, exists=True,
                               file_okay=True),
               help='Filepath to Kraken report')
@@ -122,16 +100,19 @@ def create_db_folder(repophlan_genomeid_taxonomy_fp,
                                                      'domain']),
               required=False, default=['genus'], show_default=True,
               help="Taxonomic rank at which to generate summary")
-
 @click.option('--read-per-taxa', required=False,
               default=10, show_default=True,
-              help="Minimum number of reads needed for each taxa")
-
+              help="Minimum number of reads for each taxa")
+@click.option('--output-filename', required=False,
+              default='subset_repophlan_genomeID_taxonomy.good',
+              show_default=True,
+              help="Output filename for RepoPhlAn genome ID and taxonomy list")
 
 def main(kraken_mpa_report_fp,
          repophlan_genomeid_taxonomy_fp,
          taxonomic_rank,
-         read_per_taxa):
+         read_per_taxa,
+         output_filename):
 
     taxa_levels = {"domain": "d__",
                    "phylum": "|p__",
@@ -147,19 +128,25 @@ def main(kraken_mpa_report_fp,
                        "4": "|f__", "3": "|o__", "2": "|c__",
                        "1": "|p__", "0": "d__"}
 
-    taxonomic_set, spit_on_level, repo_file_name =\
+    taxa_levels_str=taxa_levels[taxonomic_rank]
+    taxa_levels_idx_int=taxa_levels_idx[taxa_levels_str]
+
+    if taxa_levels_idx_int < 6:
+        split_on_level = taxa_levels_idx[str(taxa_levels_idx_int + 1)]
+    else:
+        split_on_level = '\t'
+
+    taxonomic_set =\
         load_kraken_mpa_report(
             kraken_mpa_report_fp=kraken_mpa_report_fp,
-            taxonomic_rank=taxonomic_rank,
-            taxa_levels=taxa_levels,
-            taxa_levels_idx=taxa_levels_idx,
+            taxa_levels=[taxa_levels_str,split_on_level],
             read_per_taxa=read_per_taxa)
 
     create_db_folder(
             repophlan_genomeid_taxonomy_fp=repophlan_genomeid_taxonomy_fp,
             genome_tax=taxonomic_set,
-            split_on_level=spit_on_level,
-            repo_file_name=repo_file_name)
+            split_on_level=split_on_level,
+            output_filename=output_filename)
 
 if __name__ == "__main__":
     main()
